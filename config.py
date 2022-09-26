25622150d462bb7138fa15a583c19ed7ec1f8b1d
annyitay <112464662+annyitay@users.noreply.github.com>
2022-09-26 21:27:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "humanresource.clidrd34hqzi.us-east-1.rds.amazonaws.com"
+customuser = "userID"
+custompass = "User112233"
+customdb = "HumanResource"
+custombucket = "tayannyi-bucket"
+customregion = "us-east-1"
